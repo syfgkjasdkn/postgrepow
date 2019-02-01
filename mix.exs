96729d419bf25a2fe751a12fd4457264b60b2b04
adsfiuqweliuf <ajsdfgu@uaydgfkajh>
2019-02-01 23:21:32 +0000

    lower required elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PostgrePow.MixProject do
     [
       app: :postgrepow,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
