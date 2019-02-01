930100489387523a2e5c98b1bd2ca8b244948730
adsfiuqweliuf <ajsdfgu@uaydgfkajh>
2019-02-01 22:24:25 +0000

    add aliases

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule PostgrePow.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -26,6 +27,13 @@ defmodule PostgrePow.MixProject do
       {:benchee, "~> 0.13.2", only: :bench},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
     ]
   end
 end
