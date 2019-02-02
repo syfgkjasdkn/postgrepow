01de37e3f5cc0584280304f795f6d11ee6435774
adsfiuqweliuf <ajsdfgu@uaydgfkajh>
2019-02-02 00:01:46 +0000

    fix test config on ci

--- config/test.exs
+++ config/test.exs
@@ -4,3 +4,9 @@ config :postgrepow, PostgrePow.Repo,
   database: "postgrepow_test",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
+
+if System.get_env("CI") do
+  config :postgrepow, PostgrePow.Repo,
+    username: System.get_env("POSTGRES_USER"),
+    password: System.get_env("POSTGRES_PASSWORD")
+end
